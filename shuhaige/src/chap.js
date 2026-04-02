f9c3b4f668f84169fa8c4d71a74b0333351bc23d
Vung Nho <vungnho@gmail.com>
2026-04-03 02:01:40 +0700

    Fix chap show for Shuhaige.net plugin

--- shuhaige/src/chap.js
+++ shuhaige/src/chap.js
@@ -4,20 +4,30 @@ function execute(url) {
 
     let response = fetch(url);
     if (response.ok) {
-        let doc = response.html();
-        let contentElement = doc.select(".truyen").first();
+        let doc;
+        let nextPart = url;
+        let content = '';
+        do {
+            console.log(nextPart)
+            doc = fetch(nextPart).html();
+            nextPart = BASE_URL + doc.select("a:contains(下一页)").first().attr("href")
+            content += doc.select("#content").html();
+        } while (nextPart != BASE_URL)
 
-        if (contentElement) {
-            contentElement.select("script, ins, div, style").remove();
-
-            let content = contentElement.html();
-
-            content = content.replace(/&nbsp;/g, " ");
-            content = content.replace(/<br\s*\/?>\s*<br\s*\/?>/g, "<br><br>");
-
-            return Response.success(content);
-        }
+        return Response.success(cleanHtml(content));
     }
-
     return null;
+}
+
+function cleanHtml(content) {
+    return content
+        .replace(/(<br>\s*){2,}/g, '<br>')
+        .replace(/<a[^>]*>([^<]+)<\/a>/g, '')
+        .replace(/&(nbsp|amp|quot|lt|gt);/g, "")
+        .replace(/<!--(<br \/>)?[^>]*-->/gm, '')
+        .replace(/\&nbsp;/g, "")
+        .replace(/<p>\s*喜欢[\s\S]*?小说网更新速度全网最快。\s*<\/p>/g,'')
+        .replace(/这章没有结束，请点击下一页继续阅读！\s*/g, '')
+        .replace(/<p>\s*小主，这个章节后面还有哦，请点击下一页继续阅读，后面更精彩！\s*<\/p>/g,'')
+        .replace(/<p>\s*本小章还未完，请点击下一页继续阅读后面精彩内容！\s*<\/p>/g,'');
 }
